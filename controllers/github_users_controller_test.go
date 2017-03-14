a34f904cafc0046ecf955980dd71d4210d33ecfb
Trevor Rosen <trevor@catapult-creative.com>
2017-03-13 22:12:20 -0500

    Bunch of presentation work and some stub tests

--- controllers/github_users_controller_test.go
+++ controllers/github_users_controller_test.go
@@ -8,6 +8,11 @@ func TestUserCreationValidPayload_NonDupe(t *testing.T) {
 }
 
 // POST /users
+// when the payload is valid, the media type returned is of the proper structure
+func TestUserCreationValidPayload_MediaTypeStructureIsValid(t *testing.T) {
+}
+
+// POST /users
 // when the payload is valid, contains correct keys and represents a duplicate, responds with 409
 func TestUserCreationValidPayload_Dupe(t *testing.T) {
 }
@@ -20,4 +25,19 @@ func TestUserCreationInValidPayload(t *testing.T) {
 // POST /users
 // when the payload can't be turned into a struct responds with 400
 func TestUserCreationBadJson(t *testing.T) {
+}
+
+// GET /users/{username}
+// when the username is in the DB, respond with 200
+func TestGetUser_Found(t *testing.T) {
+}
+
+// GET /users/{username}
+// when the username can't be found responds with 404
+func TestGetUser_NotFound(t *testing.T) {
+}
+
+// GET /users/{username}
+// when the username can be found, the app responds with the proper media type structure
+func TestGetUser_MediaTypeStructureIsValid(t *testing.T) {
 }
