ff932ca59c5ad7bc7e3ee55e107c735a84c48ef1
Trevor Rosen <trevor@catapult-creative.com>
2017-03-13 21:22:44 -0500

    Flesh out controller create method

--- controllers/github_users_controller.go
+++ controllers/github_users_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/trevrosen/git-grabber/db"
@@ -12,9 +13,9 @@ type GitHubUserCreateUserType struct {
 	Username string
 }
 
-type GitHubUserMediaTyoe struct {
+type GitHubUserMediaType struct {
 	// ID is the primary key in this database
-	ID int
+	ID uint
 	// Username is the GitHub username
 	Username string
 }
@@ -22,8 +23,37 @@ type GitHubUserMediaTyoe struct {
 // userCreateHandler handles POST /users/
 func userCreateHandler(dbi db.DBInteractor) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// create a user
+		var err error
+		input := &GitHubUserCreateUserType{}
+		decoder := json.NewDecoder(r.Body)
+		err = decoder.Decode(input)
 
+		if err != nil {
+			logMsg(r, err.Error())
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		user := &db.GitHubUser{
+			Username: input.Username,
+		}
+
+		// TODO: better status code?
+		if err = dbi.SaveRecord(user); err != nil {
+			logMsg(r, err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		// TODO: better status code?
+		jsonBytes, err := serializedGitHubUser(user)
+		if err != nil {
+			logMsg(r, err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Write(jsonBytes)
+		w.WriteHeader(http.StatusCreated)
 	})
 }
 
@@ -32,4 +62,14 @@ func userShowHandler(dbi db.DBInteractor) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// find/serialize a user
 	})
+}
+
+// serializedGitHubUser serializes a db.GitHubUser for the wire
+func serializedGitHubUser(ghUser *db.GitHubUser) ([]byte, error) {
+	jsonSource := &GitHubUserMediaType{}
+
+	jsonSource.ID = ghUser.ID
+	jsonSource.Username = ghUser.Username
+
+	return json.Marshal(jsonSource)
 }
